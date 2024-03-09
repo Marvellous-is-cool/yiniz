0ff28a14b5a53a6d857475ab7ec407c7f6a64228
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-03-09 12:11:41 +0100

    etest modified - new test

--- app.js
+++ app.js
@@ -75,6 +75,11 @@ app.use(
 app.use("/", clientRoutes);
 app.use("/admin", authRoutes);
 
+// 404
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
