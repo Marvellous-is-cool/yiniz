5e46fcaf2840e576c1040a944223390d1a8e9ed6
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-06-05 11:37:58 +0100

    Assignment Submission Project Added (v2-production)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const sessionConfig = {
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "development" ? true : false,
+    secure: process.env.NODE_ENV === "development" ? false : true,
   },
   resave: false,
   saveUninitialized: false,
