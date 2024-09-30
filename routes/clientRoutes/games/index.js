0f6e419e9628eca99535179db8169f6faa1ac66e
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-09-30 13:13:42 +0100

    Added ads, removed game2

--- routes/clientRoutes/games/index.js
+++ routes/clientRoutes/games/index.js
@@ -1,7 +1,6 @@
 // index.js
 const express = require("express");
 const router = express.Router();
-const roftfsboreRouter = require("./roftfsbore");
 
 // landing page
 router.get("/", (req, res) => {
@@ -12,8 +11,5 @@ router.get("/", (req, res) => {
 router.get("/carrush", (req, res) => {
   res.render("games/game1/carrush");
 });
-
-// roftfsbore
-router.use("/roftfsbore", roftfsboreRouter);
 
 module.exports = router;
--- routes/serverRoutes/games/index.js
+++ routes/serverRoutes/games/index.js
@@ -1,7 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const roftfsboreRouter = require("./roftfsbore/index");
-
-router.use("/roftfsbore", roftfsboreRouter);
 
 module.exports = router;
